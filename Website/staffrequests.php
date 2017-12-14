ac800e9725076f1c7e572c6de91bab84d63b9c67
d3db0t <ShadyTantawy@protonmail.com>
2017-12-14 14:08:41 +0200

    Added staffrequests webpage

---
+++ Website/staffrequests.php
@@ -0,0 +1,47 @@
+<?php 
+    session_start();
+	include('mnavbarcounts.php');
+?>
+<html>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+
+	<body>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			<a class="navbar-brand" href="#">IWork</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation" style="">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarColor01">
+				<ul class="navbar-nav mr-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="mhome.php">Home</a>
+				</li>
+				<li class="nav-item">
+                    <a class="nav-link" href="mprofile.php">Profile</a>
+                </li>
+				<li class="nav-item active">
+                    <a class="nav-link" href="staffrequests.php">StaffRequests <span class="sr-only">(current)</span><span class="badge badge-success badge-pill" style='background-color: #34B3A0;color: white'><?php echo $staffRequestsCount ?></span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="">Applications<span class="badge badge-success badge-pill" style='background-color: #34B3A0;color: white'>14</span></a>
+				</li>
+				</ul>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php" style='color: #34B3A0'><button type="button" class="btn btn-outline-primary">Logout</button></a>
+                    </li>
+                </ul>
+			</div>
+		</nav>
+
+        <?php
+            include('functions.php');
+            ViewEmployeesRequests($manager, $conn);
+            if (isset($_GET['code']))
+            {
+                ManagerAcceptRequests($manager, $_GET['code'], $conn);
+            }
+        ?>
+    </body>
+</html>
